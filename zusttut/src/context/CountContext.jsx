0718b6c096301dd7830e9cfde651b216baa7a667
Shouryya <shouryyamanna06@gmail.com>
2025-08-26 15:23:01 +0530

    Practised Zutand

--- zusttut/src/context/CountContext.jsx
+++ zusttut/src/context/CountContext.jsx
@@ -1,14 +1,15 @@
-import React from 'react';
-import {create} from 'zustand';
+import React from "react";
+import { create } from "zustand";
 
-
-const countStore = (set)=>({
-    count: 0,
-    increment: ()=>{
-        set((state)=>({count: state.count+1}))
-    },
-    decrement: ()=> set((state)=>({count:state.count-1}))
-})
+const countStore = (set) => ({
+  count: 0,
+  increment: () => {
+    set((state) => ({ count: state.count + 1 }));
+  },
+  decrement: () => {
+    set((state) => ({ count: state.count - 1 }));
+  },
+});
 
 const useCountStore = create(countStore);
 
